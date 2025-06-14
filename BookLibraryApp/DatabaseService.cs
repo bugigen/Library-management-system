8706b714fbfb44eafa96731b1ca84be97c45d020
elgrenka <bugigen@yandex.ru>
2025-06-14 16:14:16 +0400

    create database

---
+++ BookLibraryApp/DatabaseService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace BookLibraryApp
+{
+    internal class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService(string databasePath = "bookLibrary.db")
+        {
+            _connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = databasePath,
+                Mode = SqliteOpenMode.ReadWriteCreate
+            }.ToString();
+        }
+
+        public void InitializeDatabase()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = """
+                    CREATE TABLE IF NOT EXISTS Books (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Title TEXT NOT NULL,
+                        Author TEXT NOT NULL,
+                        Year INTEGER NOT NULL,
+                        Genre TEXT NOT NULL,
+                        Status TEXT NOT NULL DEFAULT 'доступна'
+                    );
+                    """;
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public SqliteConnection GetConnection() => new SqliteConnection( _connectionString );
+    }
+}
--- BookLibraryApp/Program.cs
+++ BookLibraryApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.Sqlite;
+
 namespace BookLibraryApp
 {
     internal static class Program
@@ -6,12 +8,52 @@ namespace BookLibraryApp
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            var databaseService = new DatabaseService();
+
+            try
+            {
+                databaseService.InitializeDatabase();
+                Console.WriteLine("База данных успешно создана.");
+
+                using (SqliteConnection connection = databaseService.GetConnection())
+                {
+                    connection.Open();
+
+                    var insertCommand = connection.CreateCommand();
+
+                    insertCommand.CommandText = "INSERT INTO Books (Id, Title, Author, Year, " +
+                        "Genre, Status) VALUES (1, 'Название 1', 'Автор 1', 'Год 1', 'Жанр 1', " +
+                        "'доступна')";
+
+                    int rowsAffected = insertCommand.ExecuteNonQuery();
+                    Console.WriteLine($"Добавлено записей: {rowsAffected}");
+
+                    var selectCommand = connection.CreateCommand();
+
+                    selectCommand.CommandText = "SELECT * FROM Books";
+
+                    using (var book = selectCommand.ExecuteReader())
+                    {
+                        while (book.Read())
+                        {
+                            Console.WriteLine($"ID: {book["Id"]}, Название: {book["Title"]}, " +
+                                $"Автор: {book["Author"]}, Год выпуска: {book["Year"]}, " +
+                                $"Жанр: {book["Genre"]}, Статус: {book["Status"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
         }
     }
 }
